c28014e8bec03c72795d83b1fe4970716e37b983
Dika Eka <andkmln87@gmail.com>
2023-06-27 21:04:42 +0700

    [FIX] nama guru

--- server/list-guru.js
+++ server/list-guru.js
@@ -12,6 +12,26 @@ const data = [{
     "nama" : "Taslimatul A.F, M.Ko",
     "jabatan" : "Guru Mapel",
     "foto" : "/imgs/Bu Nafa.jpg"
+},
+{
+    "nama" : "Anton Hidayat, S.Kom",
+    "jabatan" : "Guru Mapel"
+},
+{
+    "nama" : "Mara Tilovasanti, S.Pd T, M.Pd",
+    "jabatan" : "Guru Mapel"
+},
+{
+    "nama" : "Nur Linda Achmad, S.Pd. Kom",
+    "jabatan" : "Guru Mapel"
+},
+{
+    "nama" : "Sri Wahyudi, S.Pd., M.Pd.",
+    "jabatan" : "Guru Mapel"
+},
+{
+    "nama" : "Arif Basuki Wibowo, S.Pd",
+    "jabatan" : "Guru Mapel"
 }]
 
 export default data;
