d63e4b7ef70809db0521006ff37900c75a245db3
Dika Eka <andkmln87@gmail.com>
2023-08-11 19:20:19 +0700

    [ADD] Photo Guru Added

--- server/list-guru.js
+++ server/list-guru.js
@@ -15,23 +15,28 @@ const data = [{
 },
 {
     "nama" : "Anton Hidayat, S.Kom",
-    "jabatan" : "Guru Mapel"
+    "jabatan" : "Guru Mapel",
+    "foto" : "/imgs/pa anton.jpg"
 },
 {
     "nama" : "Mara Tilovasanti, S.Pd T, M.Pd",
-    "jabatan" : "Guru Mapel"
+    "jabatan" : "Guru Mapel",
+    "foto" : "/imgs/bu santi.jpg"
 },
 {
     "nama" : "Nur Linda Achmad, S.Pd. Kom",
-    "jabatan" : "Guru Mapel"
+    "jabatan" : "Guru Mapel",
+    "foto" : "pa linda.jpg"
 },
 {
     "nama" : "Sri Wahyudi, S.Pd., M.Pd.",
-    "jabatan" : "Guru Mapel"
+    "jabatan" : "Guru Mapel",
+    "foto" : "pa sri"
 },
 {
     "nama" : "Arif Basuki Wibowo, S.Pd",
-    "jabatan" : "Guru Mapel"
+    "jabatan" : "Guru Mapel",
+    "foto" : "pa arif.jpg"
 }]
 
 export default data;
